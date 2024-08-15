32c0741e48f114376525060f333996cc81539abc
tatto57 <tatto57@hotmail.com>
2024-08-15 16:13:17 -0500

    test gunicorn

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def recibir_mensajes():
         return 'no enviado ' + str(e)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
